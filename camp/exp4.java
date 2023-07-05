c8cc629c93ba9a8eabdd5262b6d3753b330f75a5
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-05 11:03:02 +0530

    4

---
+++ camp/exp4.java
@@ -0,0 +1,16 @@
+class BankAccount { static class InsufficientBalanceException extends Exception { public InsufficientBalanceException(String message) { super(message); 
+} } private double balance; public BankAccount(double initialBalance) { this.balance = initialBalance; 
+} public void deposit(double amount) { balance += amount; 
+} public void withdraw(double amount) throws InsufficientBalanceException { if (amount > balance) { 
+throw new InsufficientBalanceException("Insufficient balance in the account."); } balance -= amount; 
+} public double getBalance() { return balance; 
+} public static void main(String[] args) { BankAccount account = new BankAccount(1000.0); double withdrawAmount = 1500.0; try { account.withdraw(withdrawAmount); 
+System.out.println("Withdrawal of $" + withdrawAmount + " successful."); 
+} catch (InsufficientBalanceException e) { 
+System.out.println("Withdrawal failed: " + e.getMessage()); } catch (Exception e) { 
+System.out.println("An error occurred: " + e.getMessage()); 
+} finally { 
+System.out.println("Current balance: $" + account.getBalance()); 
+} 
+} 
+} 
