9e83ace5dae182afea7a1dfc9ea2424ed4a33966
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-06 10:38:29 +0530

    7

---
+++ camp/d7.java
@@ -0,0 +1,19 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Welcome to MyServlet!</h1>");
+        out.println("<p>This is a simple servlet example.</p>");
+        out.println("</body></html>");
+    }
+}
