573ea0e62d7d09466a15c6b9ae64bd825068f118
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-07 13:20:25 +0530

    d1

--- camp/ads1.cpp
+++ camp/ads1.cpp
@@ -5,7 +5,6 @@ void findCommon(int ar1[], int ar2[], int ar3[], int n1, int n2, int n3) {
     int i = 0, j = 0, k = 0;
     
     while (i < n1 && j < n2 && k < n3) {
-        // If x = y and y = z, print any of them and move
         if (ar1[i] == ar2[j] && ar2[j] == ar3[k]) {
             cout << ar1[i] << " ";
             i++;
@@ -18,7 +17,6 @@ void findCommon(int ar1[], int ar2[], int ar3[], int n1, int n2, int n3) {
         // y < z
         else if (ar2[j] < ar3[k])
             j++;
-        // We reach here when x > y and z < y, i.e., z is smallest
         else
             k++;
     }
