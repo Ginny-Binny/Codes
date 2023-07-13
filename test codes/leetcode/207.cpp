67e57839113fb5bf70b54152a54f1a51065398fc
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-13 15:54:23 +0530

    207

--- test codes/leetcode/207.cpp
+++ test codes/leetcode/207.cpp
@@ -7,7 +7,6 @@ public:
             adj[prerequisite[1]].push_back(prerequisite[0]);
             indegree[prerequisite[0]]++;
         }
-
         queue<int> q;
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
