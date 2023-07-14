a906b69ede9e553f942239664e3d23ef07d9b34d
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-14 23:36:09 +0530

    8

--- camp/exp2.6.py
+++ camp/exp2.6.py
@@ -1,22 +1,12 @@
 # Message Analyzer
 # Demonstrates the len() function and the in operator
-
- 
-
 message = input("Enter a message: ")
 
- 
-
 print("\nThe length of your message is:", len(message))
-
- 
 
 print("\nThe most common letter in the English language, 'and',")
 if "and" in message:
     print("is in your message.")
 else:
     print("is not in your message.")
-
- 
-
 input("\n\nPress the enter key to exit.")
