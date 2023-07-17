03298c96d77eb571b645c05615fa0b1d8a6193bd
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-17 11:09:47 +0530

    4

--- test codes/python/NEW AI 1.py
+++ test codes/python/NEW AI 1.py
@@ -6,5 +6,5 @@ dice2 = random.randrange(1, 6)
 total = dice1 + dice2
 
 print("You rolled a", dice1, "and a", dice2, "for a total of", total)
-print("\n20BCS2347_Paramjyati Mushahary")
+print("aditi mehra -- 20BCS4806")
 input("\n\nPress the enter key to exit.")
--- test codes/python/tempCodeRunnerFile.py
+++ test codes/python/tempCodeRunnerFile.py
@@ -1,6 +1,10 @@
-print("\nHealth is Wealth")
+import random
 
-print("\nAnonymous")
+dice1 = random.randint(2, 9) 
+dice2 = random.randrange(1, 6)  
 
+total = dice1 + dice2
+
+print("You rolled a", dice1, "and a", dice2, "for a total of", total)
 print("aditi mehra -- 20BCS4806")
-input("\n\nPress the enter key to exit")
+input("\n\nPress the enter key to exit.")
