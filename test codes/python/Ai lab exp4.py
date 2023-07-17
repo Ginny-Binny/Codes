d27a21c92b07b8af3b8a361a7a5c04a191a375ca
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-17 11:08:56 +0530

    4

--- test codes/python/Ai lab exp4.py
+++ test codes/python/Ai lab exp4.py
@@ -2,5 +2,5 @@ print("\nHealth is Wealth")
 
 print("\nAnonymous")
 
-print("Paramjyati Mushahary--20BCS2347")
+print("aditi mehra -- 20BCS4806")
 input("\n\nPress the enter key to exit")
--- test codes/python/tempCodeRunnerFile.py
+++ test codes/python/tempCodeRunnerFile.py
@@ -1,6 +1,6 @@
-d = input ("\n\nEnter Your Favorite Bike \n\n")
-print("\n\nYou like",d,"Color of bike")
+print("\nHealth is Wealth")
+
+print("\nAnonymous")
 
 print("aditi mehra -- 20BCS4806")
-
 input("\n\nPress the enter key to exit")
