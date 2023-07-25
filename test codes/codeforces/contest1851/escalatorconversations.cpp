abb1466d7a1893ce72753499acba424254536bc3
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 21:28:40 +0530

    1

--- test codes/codeforces/contest1851/escalatorconversations.cpp
+++ test codes/codeforces/contest1851/escalatorconversations.cpp
@@ -11,26 +11,22 @@ int main() {
     while (t--) {
         int n, m, k, H;
         cin >> n >> m >> k >> H;
-        vector<int> heights(n);
+        vector<int> h(n);
         for (int i = 0; i < n; ++i) {
-            cin >> heights[i];
+            cin >> h[i];
         }
-
-        queue<int> candidates;
-        candidates.push(1);
-        candidates.push(2);
-
-        int valid_count = 0;
-        for(int i = 0; i < n; i++) {
-            int diff = abs(H - heights[i]);
-            bool divisible = candidates.size() && (diff % k == 0);
-            bool within_range = candidates.size() && (diff <= (m - 1) * k);
-            bool not_zero_diff = candidates.size() && (diff > 0);
-            if (divisible && within_range && not_zero_diff) {
-                valid_count++;
-            }
+        queue<int> q;
+        q.push(1);
+        q.push(2);
+        int sum  =0;
+        for(int i = 0; i < n; i++){
+          int diff = abs(H - h[i]);
+          bool flag1 = q.size() && diff % k == 0;
+          bool flag2 = q.size() && diff <= (m - 1) * k;
+          bool flag3 = q.size() && diff;
+          if(q.size() && flag1 && flag2 && flag3) sum++;
         }
-        cout << valid_count << endl;
+        cout << sum << endl;
     }
 
     return 0;
