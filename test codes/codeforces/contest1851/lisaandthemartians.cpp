dd978c05c02d990643af5368c2fee0e669371a0c
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 22:07:06 +0530

    f

--- test codes/codeforces/contest1851/lisaandthemartians.cpp
+++ test codes/codeforces/contest1851/lisaandthemartians.cpp
@@ -8,18 +8,38 @@ void findOptimalIndicesAndX(int n, int k, const vector<int>& martian_numbers) {
     int max_value = -1;
     int i = 0, j = 0, x = 0;
 
-    for (int x_iter = 0; x_iter < (1 << k); x_iter++) {
-        int current_x = x_iter;
+    // Precompute the XOR of all possible pairs and store in a 2D matrix
+    vector<vector<int>> xor_pairs(n, vector<int>(n, 0));
+    for (int i_idx = 0; i_idx < n; i_idx++) {
+        for (int j_idx = i_idx + 1; j_idx < n; j_idx++) {
+            xor_pairs[i_idx][j_idx] = xor_pairs[j_idx][i_idx] = martian_numbers[i_idx] ^ martian_numbers[j_idx];
+        }
+    }
+
+    // Iterate through the bits of x from MSB to LSB
+    for (int bit = k - 1; bit >= 0; bit--) {
+        // Check if flipping the current bit increases the maximum value
+        int new_max_value = max_value;
+        int new_i = i, new_j = j, new_x = x;
+
         for (int i_idx = 0; i_idx < n; i_idx++) {
             for (int j_idx = i_idx + 1; j_idx < n; j_idx++) {
-                int value = (martian_numbers[i_idx] ^ current_x) & (martian_numbers[j_idx] ^ current_x);
-                if (value > max_value) {
-                    max_value = value;
-                    i = i_idx + 1; // Convert to 1-based index
-                    j = j_idx + 1; // Convert to 1-based index
-                    x = current_x;
+                int value = xor_pairs[i_idx][j_idx] ^ (x ^ (1 << bit));
+                if (value > new_max_value) {
+                    new_max_value = value;
+                    new_i = i_idx + 1; // Convert to 1-based index
+                    new_j = j_idx + 1; // Convert to 1-based index
+                    new_x = x ^ (1 << bit);
                 }
             }
+        }
+
+        // Update the maximum value and related variables if needed
+        if (new_max_value > max_value) {
+            max_value = new_max_value;
+            i = new_i;
+            j = new_j;
+            x = new_x;
         }
     }
 
