58774d95b95ab0149d43e995063eb5b4e0b428e0
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 21:29:40 +0530

    1

--- test codes/codeforces/contest1851/escalatorconversations.cpp
+++ test codes/codeforces/contest1851/escalatorconversations.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define int long long
 using namespace std;
 
 int main() {
@@ -18,13 +17,14 @@ int main() {
         queue<int> q;
         q.push(1);
         q.push(2);
-        int sum  =0;
-        for(int i = 0; i < n; i++){
-          int diff = abs(H - h[i]);
-          bool flag1 = q.size() && diff % k == 0;
-          bool flag2 = q.size() && diff <= (m - 1) * k;
-          bool flag3 = q.size() && diff;
-          if(q.size() && flag1 && flag2 && flag3) sum++;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = abs(H - h[i]);
+            bool flag1 = q.size() && diff % k == 0;
+            bool flag2 = q.size() && diff <= (m - 1) * k;
+            bool flag3 = q.size() && diff;
+            if (q.size() && flag1 && flag2 && flag3)
+                sum++;
         }
         cout << sum << endl;
     }
