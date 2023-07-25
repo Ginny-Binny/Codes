ec28bdb7c3e2ea547be217deb1009e2f304f28e0
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 22:05:10 +0530

    ques f

--- test codes/codeforces/contest1851/lisaandthemartians.cpp
+++ test codes/codeforces/contest1851/lisaandthemartians.cpp
@@ -6,16 +6,18 @@ using namespace std;
 // Function to find optimal indices i, j, and x
 void findOptimalIndicesAndX(int n, int k, const vector<int>& martian_numbers) {
     int max_value = -1;
-    int i = 1, j = 2, x = 0;
+    int i = 0, j = 0, x = 0;
 
-    for (x = 0; x < (1 << k); x++) {
+    for (int x_iter = 0; x_iter < (1 << k); x_iter++) {
+        int current_x = x_iter;
         for (int i_idx = 0; i_idx < n; i_idx++) {
             for (int j_idx = i_idx + 1; j_idx < n; j_idx++) {
-                int value = (martian_numbers[i_idx] ^ x) & (martian_numbers[j_idx] ^ x);
+                int value = (martian_numbers[i_idx] ^ current_x) & (martian_numbers[j_idx] ^ current_x);
                 if (value > max_value) {
                     max_value = value;
                     i = i_idx + 1; // Convert to 1-based index
                     j = j_idx + 1; // Convert to 1-based index
+                    x = current_x;
                 }
             }
         }
