20a3c4cda52ac92667d080ab4a65ee1f8c467acf
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 21:27:53 +0530

    1

--- test codes/codeforces/contest1851/escalatorconversations.cpp
+++ test codes/codeforces/contest1851/escalatorconversations.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-
+#include <queue>
+#define int long long
 using namespace std;
 
 int main() {
@@ -10,25 +11,26 @@ int main() {
     while (t--) {
         int n, m, k, H;
         cin >> n >> m >> k >> H;
-
         vector<int> heights(n);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             cin >> heights[i];
         }
 
-        int conversations = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int stepHeight = (j + 1) * k;
-                int heightDiff = abs(heights[i] - stepHeight);
-                if (heightDiff > 0 && heightDiff <= H && heightDiff % k == 0) {
-                    conversations++;
-                    break; // Move to the next person
-                }
+        queue<int> candidates;
+        candidates.push(1);
+        candidates.push(2);
+
+        int valid_count = 0;
+        for(int i = 0; i < n; i++) {
+            int diff = abs(H - heights[i]);
+            bool divisible = candidates.size() && (diff % k == 0);
+            bool within_range = candidates.size() && (diff <= (m - 1) * k);
+            bool not_zero_diff = candidates.size() && (diff > 0);
+            if (divisible && within_range && not_zero_diff) {
+                valid_count++;
             }
         }
-
-        cout << conversations << endl;
+        cout << valid_count << endl;
     }
 
     return 0;
