d808e7c009f818959d6d84a97271721d94f8a738
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-07-25 20:51:34 +0530

    1851

---
+++ test codes/codeforces/contest1851/escalatorconversations.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m, k, H;
+        cin >> n >> m >> k >> H;
+
+        vector<int> heights(n);
+        for (int i = 0; i < n; i++) {
+            cin >> heights[i];
+        }
+
+        int conversations = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int stepHeight = (j + 1) * k;
+                int heightDiff = abs(heights[i] - stepHeight);
+                if (heightDiff == 0 || heightDiff % k == 0) {
+                    conversations++;
+                    break; // Move to the next person
+                }
+            }
+        }
+
+        cout << conversations << endl;
+    }
+
+    return 0;
+}
