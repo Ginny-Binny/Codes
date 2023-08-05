1205096eeb1eaf6f1dfa4ac6956c466bfddd6631
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-05 21:33:41 +0530

    1

---
+++ test codes/leetcode/biweekly conteset 110/accountbalanceafterroundedpurchase.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int nearestMultipleOf10(int num) {
+        return ((num + 5) / 10) * 10; // Round to the nearest multiple of 10
+    }
+    
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        int accountBalance = 100; // Initial account balance
+        int roundedAmount = nearestMultipleOf10(purchaseAmount);
+        int newBalance = accountBalance - roundedAmount;
+        return newBalance;
+    }
+};
