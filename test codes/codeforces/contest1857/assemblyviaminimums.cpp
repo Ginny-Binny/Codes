23cac4ff407b31c7c120f1874e6ecd36bd73bee7
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-07 23:08:22 +0530

    3

---
+++ test codes/codeforces/contest1857/assemblyviaminimums.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+const int N= 200005;
+const int MOD= 1000003;
+const int sz= 1e3+1;
+#define ll long long;
+
+
+signed main(){
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+        ll m= (n*(n-1))/2;
+        map<ll,ll> mp;
+        ll a[m];
+        for(int i=0; i<m;i++){
+            cin>>a[i];
+
+        }
+        mp[a[i]]++;
+
+    }
+    ll r=n-1;
+    vector<ll>v;
+    for(auto it: mp){
+        if(r==it.second){
+            v.push_back(it.first);
+            r--;
+        }
+        else{
+            ll x=r,p=0;
+            while(x>0 &&it.second!=p){
+                p +=(x);
+                v.push_back(it.first);
+                x--;
+
+            }
+            r=x;
+        }
+    }
+    for(auto it:v){
+        cout<<it<<'';
+    }
+    int x=v.size();
+    while(x!=n){
+        cout<<1000000000<<'';
+        x++;
+    }
+    cout<<endl;
+}
+return 0;
