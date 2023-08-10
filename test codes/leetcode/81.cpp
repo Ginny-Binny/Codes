2d674c7d222596a7a732c8132360329584caf801
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-11 00:44:25 +0530

    1

--- test codes/leetcode/81.cpp
+++ test codes/leetcode/81.cpp
@@ -38,7 +38,6 @@ public:
         return false;
     }
 
-    // returns true if we can reduce the search space in current binary search space
     bool isBinarySearchHelpful(vector<int>& nums, int start, int element) {
         return nums[start] != element;
     }
