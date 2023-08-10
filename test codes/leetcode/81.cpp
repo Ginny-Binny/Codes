a400e012c7a08797e76a7c20e9ab93283cf99812
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-11 00:47:58 +0530

    Search in Rotated Sorted Array II

--- test codes/leetcode/81.cpp
+++ test codes/leetcode/81.cpp
@@ -42,7 +42,6 @@ public:
         return nums[start] != element;
     }
 
-    // returns true if element exists in first array, false if it exists in second
     bool existsInFirst(vector<int>& nums, int start, int element) {
         return nums[start] <= element;
     }
