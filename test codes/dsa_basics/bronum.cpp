4ff604403089a8fe182f35a721caf45d22e4e4ef
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-26 17:31:58 +0530

    dsa_basics

--- test codes/dsa_basics/bronum.cpp
+++ test codes/dsa_basics/bronum.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main(){
     int bronum;
+    // cin>>bronum;
+    // if(bronum==0){
+    //     cout<<"Baat bn jayegi";
+    // } 
+    // else
+    // cout<<"Baat nhi banegi";
+    if(cout<<"hi")
     cin>>bronum;
-    if(bronum==0){
-        cout<<"Baat bn jayegi";
-    } 
-    else
-    cout<<"Baat nhi banegi";
+
 return 0;
 }
