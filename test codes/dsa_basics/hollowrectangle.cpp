a9eda59cd9f01cc27ccae7f9df22642f0203d001
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-26 19:30:00 +0530

    dsa_basics

--- test codes/dsa_basics/hollowrectangle.cpp
+++ test codes/dsa_basics/hollowrectangle.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    for(int row=0;row<3;row++){
-        if(row==0|| row==2){
-            for(int col=0;col<5;col++){
-                cout<<"* ";
+    for(int row=0;row<6;row++){
+        if(row==0|| row==5){
+            for(int col=0;col<6;col++){
+                cout<<"*";
             }
         }
         else{
-            cout<<"* ";
+            cout<<"*";
             for(int a=0;a<3;a++){
                 cout<<" ";
             }
-            cout<<"* ";
+            cout<<"*";
         }
         
         cout<<endl;
