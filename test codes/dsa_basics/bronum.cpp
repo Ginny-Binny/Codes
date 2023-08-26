6e872636860ab96bc2e1a2e2ae15effd6ae48fa6
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-08-26 17:18:33 +0530

    dsa_basics

---
+++ test codes/dsa_basics/bronum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int bronum;
+    cin>>bronum;
+    if(bronum==0){
+        cout<<"Baat bn jayegi";
+    } 
+    else
+    cout<<"Baat nhi banegi";
+return 0;
+}
