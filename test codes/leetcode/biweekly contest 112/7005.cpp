d99fb16637295d619769d4652d0e0a743876bdbf
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-09-02 20:40:43 +0530

    ques 2

---
+++ test codes/leetcode/biweekly contest 112/7005.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Solution { 
+public: 
+     
+    bool checkStrings(string s1, string s2) { 
+        int n = s1.size(); 
+        string temp1; 
+        string temp2; 
+        string temp3; 
+        string temp4; 
+        for(int i=0;i<n;i++){ 
+            if(i%2){ 
+                temp1.push_back(s1[i]); 
+                temp2.push_back(s2[i]); 
+            } 
+            else{ 
+                temp3.push_back(s1[i]); 
+                temp4.push_back(s2[i]); 
+            } 
+        } 
+        sort(temp1.begin(),temp1.end()); 
+        sort(temp2.begin(),temp2.end()); 
+        sort(temp3.begin(),temp3.end()); 
+        sort(temp4.begin(),temp4.end()); 
+        if(temp1 == temp2 && temp3 == temp4) 
+            return true; 
+        else 
+            return false; 
+    } 
+};
+
