a8b4a2da5e675a6c5b58e8a0c45189c9cbcef9cb
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-09-16 19:10:10 +0530

    generalhollowrectangle

--- test codes/dsa_basics/generalhollowrectangle.cpp
+++ test codes/dsa_basics/generalhollowrectangle.cpp
@@ -3,24 +3,25 @@ using namespace std;
 int main(){
     int rownum, colnum;
     cin>>rownum>>colnum;
-    for( int row=0;row<rownum;row=row+1){
-        if(row==0 || row= rownum-1){
-            for(int col=0; col=colnum;col=col+1){
-                cout<<"*";
+    for( int row=0; row<rownum; row=row+1){
+        if(row==0 || row== rownum-1){
+            for(int col=0; col<colnum;col=col+1){
+                cout<<"* ";
             }
 
         }
         else
         {
-            cout<<"*";
-            for(int col=0;col=colnum-2;col=col+1){
-                cout<<" ";
+            cout<<"* ";
+
+            for(int i=0;i<colnum-2;i=i+1){
+                cout<<"  ";
+         
             }
+
+            cout<<"* ";
         }
+        cout<<endl;
     }
 
-
-
-
-return 0;
 }
