64b9cbc4b82c1141bca84f131dda6a14fc1980d0
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-09-16 22:54:12 +0530

    hollowdiamond

---
+++ test codes/dsa_basics/hollowdiamond.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+//top triangle
+    for(int row=0;row<n;row++){
+        for(int col=0; col<n-row-1; col++){
+            cout<<" ";
+        }
+        for(int col=0; col<2*row+1; col++){
+            if(col==0|| col==2*row){
+                cout<<"*";
+                
+            }
+            
+            else
+            cout<<" ";
+            
+        }
+        cout<<endl;
+    } 
+
+//lower triangle
+    for(int row=0;row<n;row++){
+        for(int col=0;col<row;col++){
+            cout<<" ";
+        }
+
+        for(int col=0;col<2*n-2*row-1;col++){
+            if(col==0||col==2*n-2*row-2){
+                cout<<"*";
+            }
+            else
+            cout<<" ";
+        }cout<<endl;
+    }
+    
+    
+
+}
---
+++ test codes/dsa_basics/invertedfullpyramid.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int row=0; row<n;row++ ){
+        // stars
+
+        for(int col=0; col<row;col++){
+            cout<<" ";
+
+        }
+
+        for(int col=0;col<n-row;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    } 
+return 0;
+}
