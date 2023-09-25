ca51e747f143286df0f03de720edd5eaf1d7a64f
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-09-25 18:45:39 +0530

    ques1

--- test codes/sahaj.ai/ques1.cpp
+++ test codes/sahaj.ai/ques1.cpp
@@ -3,38 +3,48 @@
 
 using namespace std;
 
+pair<int, int> findPosition(const vector<vector<int>>& flowerPots, int H) {
+    int L = flowerPots.size();
+    int W = flowerPots[0].size();
+
+    for (int i = 0; i < L; ++i) {
+        if (flowerPots[i][0] > H) {
+            return {i, 0};
+        }
+        if (flowerPots[i][W-1] >= H) {
+            // Binary search in this row
+            int l = 0, r = W - 1;
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (flowerPots[i][mid] == H) {
+                    return {i, mid};
+                } else if (flowerPots[i][mid] > H) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return {i, l};  // Insert at the found position
+        }
+    }
+    return {-1, -1};  // Should never reach this if room is full.
+}
+
 int main() {
-    int L, W;
+    int L, W, H;
     cin >> L >> W;
 
-    vector<vector<int>> room(L, vector<int>(W));
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < W; j++) {
-            cin >> room[i][j];
+    vector<vector<int>> flowerPots(L, vector<int>(W));
+    for (int i = 0; i < L; ++i) {
+        for (int j = 0; j < W; ++j) {
+            cin >> flowerPots[i][j];
         }
     }
 
-    int newHeight;
-    cin >> newHeight;
+    cin >> H;
 
-    int row = -1;
-    int col = -1;
-
-    for (int i = 0; i < L; i++) {
-        for (int j = 0; j < W; j++) {
-            if (room[i][j] > newHeight) {
-                row = i;
-                col = j;
-                break;
-            }
-        }
-        if (row != -1) {
-            break;
-        }
-    }
-
-    
-    cout << (row ) << " " << (col) << endl;
+    pair<int, int> position = findPosition(flowerPots, H);
+    cout << position.first << " " << position.second << endl;
 
     return 0;
 }
