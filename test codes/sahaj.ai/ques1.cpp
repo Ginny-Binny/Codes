2397fb5c71db40f98660287a68c51e22c48b3ac9
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-09-25 18:52:27 +0530

    q1

--- test codes/sahaj.ai/ques1.cpp
+++ test codes/sahaj.ai/ques1.cpp
@@ -11,23 +11,16 @@ pair<int, int> findPosition(const vector<vector<int>>& flowerPots, int H) {
         if (flowerPots[i][0] > H) {
             return {i, 0};
         }
-        if (flowerPots[i][W-1] >= H) {
-            // Binary search in this row
-            int l = 0, r = W - 1;
-            while (l <= r) {
-                int mid = l + (r - l) / 2;
-                if (flowerPots[i][mid] == H) {
-                    return {i, mid};
-                } else if (flowerPots[i][mid] > H) {
-                    r = mid - 1;
-                } else {
-                    l = mid + 1;
-                }
+        if (flowerPots[i][W-1] < H) {
+            continue;
+        }
+        for (int j = 0; j < W; ++j) {
+            if (flowerPots[i][j] > H) {
+                return {i, j};
             }
-            return {i, l};  // Insert at the found position
         }
     }
-    return {-1, -1};  // Should never reach this if room is full.
+    return {L-1, W-1};  // If it reaches here, replace the last pot.
 }
 
 int main() {
